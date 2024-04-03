6a744568fd1f0eb8f0c388950a700570390f0d72
Никита Погорелов <nikipog@mail.ru>
2024-04-03 18:23:53 +0300

    feat: add api service and hook it up to store

---
+++ src/services/api.ts
@@ -0,0 +1,27 @@
+import type { AxiosInstance } from 'axios';
+
+import axios from 'axios';
+
+import { getToken } from './token';
+
+const enum Default {
+  BaseUrl = 'https://15.design.htmlacademy.pro/six-cities',
+  Timeout = 5000,
+}
+
+export const createApi = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: Default.BaseUrl as string,
+    timeout: Default.Timeout as number,
+  });
+
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+    if (token && config.headers) {
+      config.headers['X-Token'] = token;
+    }
+    return config;
+  });
+  return api;
+};
+
---
+++ src/services/token.ts
@@ -0,0 +1,10 @@
+const AUTH_TOKEN_KEY = 'six-cities';
+
+export type Token = string;
+
+const getToken = (): Token => localStorage.getItem(AUTH_TOKEN_KEY) ?? '';
+const saveToken = (token : Token) => localStorage.setItem(AUTH_TOKEN_KEY, token);
+const dropToken = () => localStorage.removeItem(AUTH_TOKEN_KEY);
+
+export {getToken, saveToken, dropToken};
+
