b1d384f55f0cf0eb9da19ed3338ebcac6bf5ad1f
Никита Погорелов <nikipog@mail.ru>
2024-04-09 14:43:22 +0300

    feat: add case for empty favorites

--- src/components/favorites-item/favorites-item.tsx
+++ src/components/favorites-item/favorites-item.tsx
@@ -26,13 +26,11 @@ function FavoritesItem({ offer }: FavoritesItemProps): JSX.Element {
             <b className="place-card__price-value">&euro;{offer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
-            <FavoriteButton
-              offerId={offer.id}
-              bemBlock="place-card"
-              width={18} // Обычные размеры для карточки в избранном
-            />
-          </button>
+          <FavoriteButton
+            offerId={offer.id}
+            bemBlock="place-card"
+            width={18} // Обычные размеры для карточки в избранном
+          />
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
--- src/components/favorites-list/favorites-list.tsx
+++ src/components/favorites-list/favorites-list.tsx
@@ -1,8 +1,13 @@
 import type { ServerOffer } from '../../types/offer';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 import FavoritesItem from '../favorites-item/favorites-item';
 import { AppRoute } from '../../const';
+import { FavoritesEmpty } from '../favorites-empty/favorites-empty';
+
+const FAVORITES_IS_EMPTY = 0;
+
 
 export function FavoritesList({ offers }: { offers: ServerOffer[] }) {
   const cities: string[] = [];
@@ -21,36 +26,37 @@ export function FavoritesList({ offers }: { offers: ServerOffer[] }) {
   }
   return (
 
-    <main className="page__main page__main--favorites">
+    <main className={classNames('page__main page__main--favorites', { 'page__main--favorites-empty': offers.length === FAVORITES_IS_EMPTY })}>
       <Helmet>
         <title>6 Cities. Favorites</title>
       </Helmet>
-      <div className="page__favorites-container container">
-        <section className="favorites">
-          <h1 className="favorites__title">Saved listing</h1>
-          <ul className="favorites__list">
-            {cities.map((city) => (
-              <li className="favorites__locations-items" key ={city}>
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <Link
-                      className="locations__item-link"
-                      to={AppRoute.Main}
-                    >
-                      <span>{city}</span>
-                    </Link>
+      {offers.length === FAVORITES_IS_EMPTY ? <FavoritesEmpty /> :
+        <div className="page__favorites-container container">
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <ul className="favorites__list">
+              {cities.map((city) => (
+                <li className="favorites__locations-items" key={city}>
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <Link
+                        className="locations__item-link"
+                        to={AppRoute.Main}
+                      >
+                        <span>{city}</span>
+                      </Link>
+                    </div>
                   </div>
-                </div>
-                <div className="favorites__places">
-                  {offersByCity[city].map((offer) => (
-                    <FavoritesItem offer = {offer} key={offer.id} />
-                  ))}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </div>
+                  <div className="favorites__places">
+                    {offersByCity[city].map((offer) => (
+                      <FavoritesItem offer={offer} key={offer.id} />
+                    ))}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>}
     </main>
   );
 }
