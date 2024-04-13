ef60113972544ca9f711e1d6268635f3f9bfcdb6
Никита Погорелов <nikipog@mail.ru>
2024-04-13 15:06:09 +0300

    style: delete group-by polyfill

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app.tsx';
-import './polyfills';
 import { Provider } from 'react-redux';
 import { store } from './store/index.ts';
 
